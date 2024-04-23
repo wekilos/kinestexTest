152a2cfeb65ab331e2b12cb8b4d68ce6f0dd57dc
wekilos <wekilyazgulyyew2000@gmail.com>
2024-04-23 09:33:50 +0500

    edit

--- src/App.tsx
+++ src/App.tsx
@@ -179,8 +179,9 @@ const App: React.FC = () => {
             </div>
           </div>
           <button onClick={() => prev()} className='fixed z-50 left-2 top-[45%] text-[#464646] bg-[#0000003d] w-[40px] h-[40px] rounded-[100%] leading-[40px] text-center font-[700]'>&lt;</button>
-          <video onDoubleClick={() => console.log("disabled")}
-            onContextMenu={() => console.log("disabled")} src={URL.createObjectURL(blobed[indexOf])} autoPlay muted className="h-[100vh] absolute top-0 z-10 object-contain"></video>
+          <video controls controlslist="nofullscreen" autoPlay muted className="w-full absolute top-0 z-10 object-contain">
+            <source src={URL.createObjectURL(blobed[indexOf])}></source>
+          </video>
 
           <button onClick={() => next()} className='fixed z-50 right-2 top-[45%] text-[#464646] bg-[#0000003d] w-[40px] h-[40px] rounded-[100%] leading-[40px] text-center font-[700]'>&gt;</button>
 
@@ -188,7 +189,9 @@ const App: React.FC = () => {
           : <div className="flex flex-wrap justify-evenly">
             {blobed.map((url, index) => (
               <div key={index} className="w-24 h-32 bg-gray-200 m-1">
-                <video src={URL.createObjectURL(url)} className="w-full h-full object-cover"></video>
+                <video className="w-full h-full object-cover">
+                  <source src={URL.createObjectURL(url)}></source>
+                </video>
               </div>
             ))}
           </div>
